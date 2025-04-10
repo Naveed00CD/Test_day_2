52bd58aea4b8a32653526e99b32cea09e870e9c9
Naveed00CD <168195627+Naveed00CD@users.noreply.github.com>
2025-04-10 23:15:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+  client = boto3.client('ec2')
+  response = client.run_instances(
+    ImageId='ami-0614680123427b75e',
+    InstanceType='t2.micro',
+<<<<<<< HEAD
+    KeyName='from ec_2 8:54 test 2',
+    MaxCount=2
+    MinCount=2
+)
+
